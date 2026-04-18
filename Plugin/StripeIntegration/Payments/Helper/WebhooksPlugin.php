ea646b9bb38f6b94fcaf9b9e72e311b423fc7ce9
Aimane Couissi <contact@aimanecouissi.com>
2026-04-18 23:27:36 +0100

    Initial commit

---
+++ Plugin/StripeIntegration/Payments/Helper/WebhooksPlugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Aimane Couissi - https://aimanecouissi.com
+ * Copyright © Aimane Couissi 2026–present. All rights reserved.
+ * Licensed under the MIT License. See LICENSE for details.
+ */
+
+declare(strict_types=1);
+
+namespace AimaneCouissi\StripeWebhookSignatureBypass\Plugin\StripeIntegration\Payments\Helper;
+
+use StripeIntegration\Payments\Helper\Webhooks;
+
+class WebhooksPlugin
+{
+    /**
+     * Bypasses webhook signature verification for local testing.
+     *
+     * @param Webhooks $subject
+     * @param callable $proceed
+     * @return void
+     */
+    public function aroundVerifyWebhookSignature(Webhooks $subject, callable $proceed): void
+    {
+    }
+}
